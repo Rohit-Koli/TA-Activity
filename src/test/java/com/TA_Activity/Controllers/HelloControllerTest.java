fc440fde43e34aad3c62cd31da744b3cb538301d
Rohit Koli LAB <kolirohit1252@gmail.com>
2024-09-23 15:47:15 +0530

    Added some dependency and implemented some test strategies

---
+++ src/test/java/com/TA_Activity/Controllers/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.TA_Activity.Controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	 @Test
+	    public void testGetUser() throws Exception {
+	        mockMvc.perform(get("/api/users/1"))  // Perform GET request
+	               .andExpect(status().isOk())   // Assert status 200
+	               .andExpect(content().string("User1"));  // Assert response content
+	    }
+}
